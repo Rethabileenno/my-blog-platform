86667235dc443c58e0379a4736662a430a813677
Rethabileenno <rethabileenno@gmail.com>
2024-05-20 15:02:17 +0200

    update

--- myblog-backend/server.js
+++ myblog-backend/server.js
@@ -1,83 +1,82 @@
-// myblog-backend/server.js
-
+// Import the express module
 const express = require('express');
 const fs = require('fs');
+// Create an instance of express
 const app = express();
+// Create an instance of express with options and configuration options for the application component
+app.use(express.json());
 
 
+const blogPostsFile = './blogPosts.json';
 
 
+const blogPosts = [];
 
+fs.writeFile('./blogPosts.json', JSON.stringify(blogPosts), (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+    } else {
+        console.log('blogPosts.json has been created with an empty array.');
+    }
+});
 
+app.get('/blogPosts', (req, res) => {
+    fs.readFile(blogPostsFile, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading blog posts');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 
+app.post('/blogPosts', (req, res) => {
+    const newPost = req.body;
+    fs.readFile(blogPostsFile, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading blog posts');
+        } else {
+            const blogPosts = JSON.parse(data);
+            blogPosts.push(newPost);
+            fs.writeFile(blogPostsFile, JSON.stringify(blogPosts), (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error writing blog posts');
+                } else {
+                    res.status(201).json(newPost);
+                }
+            });
+        }
+    });
+});
 
+app.delete('/blogPosts/:id', (req, res) => {
+    const { id } = req.params;
+    fs.readFile(blogPostsFile, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading blog posts');
+        } else {
+            let blogPosts = JSON.parse(data);
+            const initialLength = blogPosts.length;
+            blogPosts = blogPosts.filter((post) => post.id !== id);
+            if (initialLength === blogPosts.length) {
+                res.status(404).send('Post not found');
+            } else {
+                fs.writeFile(blogPostsFile, JSON.stringify(blogPosts), (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('Error writing blog posts');
+                    } else {
+                        res.status(204).send();
+                    }
+                });
+            }
+        }
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.use(express.json());
-
-// // An array to store blog posts
-// let posts = [];
-
-// app.get('/api/posts', (req, res) => {
-//     res.json(posts);
-// });
-
-// app.post('/api/posts', (req, res) => {
-//     const post = req.body;
-//     posts.push(post);
-//     res.status(201).json(post);
-// });
-
-
-
-// // An array to store comments for each blog post
-// let comments = {};
-
-// app.get('/api/posts/:id', (req, res) => {
-//     const postId = Number(req.params.id);
-//     const post = posts.find(post => post.id === postId);
-//     if (post) {
-//         res.json(post);
-//     } else {
-//         res.status(404).send('Post not found');
-//     }
-// });
-
-// app.post('/api/posts/:id/comments', (req, res) => {
-//     const postId = req.params.id;
-//     const comment = req.body;
-//     if (comments[postId]) {
-//         comments[postId].push(comment);
-//     } else {
-//         comments[postId] = [comment];
-//     }
-//     res.status(201).json(comment);
-// });
-
-// app.get('/api/posts/:id/comments', (req, res) => {
-//     const postId = req.params.id;
-//     res.json(comments[postId] || []);
-// });
-
-// app.listen(3000, () => console.log('Server running on port 3000'));
+const port = 3000;
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
