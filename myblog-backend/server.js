dd56792b9b03841fda14d583dce9314687cf809b
Rethabileenno <rethabileenno@gmail.com>
2024-05-31 11:05:27 +0200

    latest updates

--- myblog-backend/server.js
+++ myblog-backend/server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://my-blog-platform-1.onrender.com'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
