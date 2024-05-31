78cafe8e9662e21a8cbbe5b191f0ca0c50bd770c
Rethabileenno <rethabileenno@gmail.com>
2024-05-31 20:22:04 +0200

    latest updates

--- myblog-backend/server.js
+++ myblog-backend/server.js
@@ -21,7 +21,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, 'uploads/'));
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
