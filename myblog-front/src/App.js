0220f1502c1aa4bda796078119d81bbef02b57ce
Rethabileenno <rethabileenno@gmail.com>
2024-05-31 19:02:43 +0200

    latest updates

--- myblog-front/src/App.js
+++ myblog-front/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import PostPage from './components/PostPage';
-import NewPostForm from './components/NewPostForm';
+import NewPostForm from './components/NewPostForm'; 
 
 
 function App() {
